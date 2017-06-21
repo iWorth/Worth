2b4decba05e26829f1bd30d4cdf32ca5f1e9e34b
iWorth <juandediostec@gmail.com>
2017-06-21 14:34:53 -0600

    Commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hola Worth");
+        primaryStage.setTitle("Hi Worth");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
